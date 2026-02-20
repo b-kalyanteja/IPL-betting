dfb609fcb3a7edcaa0fe17de900e0265731abf18
Kalyan Teja <kalyan.bulla@digitalturbine.com>
2026-02-20 22:14:17 +0100

    added player restrictions

--- utils/verify_user.py
+++ utils/verify_user.py
@@ -1,6 +1,6 @@
 
 def verify_user(email):
-    players = ["b.kalyanteja@gmail.com", "mvr08626@gmail.com"]
+    players = ["b.kalyanteja@gmail.com", "mvr08626@gmail.com", "sravanteja10@gmail.com", "narasimharao416@gmail.com"]
     if email not in players:
         st.error(f"🚫 Access Denied: {email} is not a player")
         st.stop()
