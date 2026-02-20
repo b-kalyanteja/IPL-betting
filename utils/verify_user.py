a9ec6e26f5e7964f4f790d31b2c69be09278380f
Kalyan Teja <kalyan.bulla@digitalturbine.com>
2026-02-20 22:16:34 +0100

    added player restrictions

--- utils/verify_user.py
+++ utils/verify_user.py
@@ -1,7 +1,8 @@
+import streamlit as st
 
 def verify_user(email):
     players = ["b.kalyanteja@gmail.com", "mvr08626@gmail.com", "sravanteja10@gmail.com", "narasimharao416@gmail.com"]
     if email not in players:
         st.error(f"🚫 Access Denied: {email} is not a player")
         st.stop()
-     return True
+    return True
