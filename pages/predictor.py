db2e1e588fdb503630bfcfdb2e9d5df9097cd277
Kalyan Teja <kalyan.bulla@digitalturbine.com>
2026-03-04 20:23:34 +0100

    adding predictor's picture

--- pages/predictor.py
+++ pages/predictor.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils.predictor_hall_of_fame import hall_of_fame #predictor_stats
 from utils.prediction import values_2026, percent_2026
 
-from utils.sheets_data import df_07
+from utils.sheets_data import df_04
 
 
 st.set_page_config(
@@ -22,7 +22,7 @@ st.divider()
 
 def predictor_statsss():
 
-    prediction_results= df_07.iloc[0:77, 7]
+    prediction_results= df_04.iloc[0:77, 7]
     r_list: list = prediction_results.tolist()
     st.write(r_list)
 
