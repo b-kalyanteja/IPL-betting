33465efec94d0f4752107eafa810422d1588cfaf
Kalyan Teja <kalyan.bulla@digitalturbine.com>
2026-03-05 23:43:03 +0100

    added Match poll display on main page

--- utils/logos.py
+++ utils/logos.py
@@ -3,11 +3,11 @@ logos_map = {
     "csk": "https://upload.wikimedia.org/wikipedia/en/2/2b/Chennai_Super_Kings_Logo.svg",
     "rcb": "https://upload.wikimedia.org/wikipedia/en/d/d4/Royal_Challengers_Bengaluru_Logo.svg",
     "kkr": "https://upload.wikimedia.org/wikipedia/en/4/4c/Kolkata_Knight_Riders_Logo.svg",
-    "rr": "https://upload.wikimedia.org/wikipedia/en/6/60/Rajasthan_Royals_Logo.svg",
+    "rr": "https://upload.wikimedia.org/wikipedia/en/5/5c/This_is_the_logo_for_Rajasthan_Royals%2C_a_cricket_team_playing_in_the_Indian_Premier_League_%28IPL%29.svg",
     "gt": "https://upload.wikimedia.org/wikipedia/en/0/09/Gujarat_Titans_Logo.svg",
-    "lsg": "https://upload.wikimedia.org/wikipedia/en/a/a9/Lucknow_Super_Giants_Logo.svg",
-    "dc": "https://upload.wikimedia.org/wikipedia/en/2/24/Delhi_Capitals_Logo_2023.svg",
+    "lsg": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Lucknow_Super_Giants.webp/300px-Lucknow_Super_Giants.webp.png",
+    "dc": "https://upload.wikimedia.org/wikipedia/en/2/2f/Delhi_Capitals.svg",
     "pbks": "https://upload.wikimedia.org/wikipedia/en/d/d4/Punjab_Kings_Logo.svg",
-    "srh": "https://upload.wikimedia.org/wikipedia/en/8/81/Sunrisers_Hyderabad_IPL_Logo.svg",
+    "srh": "https://upload.wikimedia.org/wikipedia/en/5/51/Sunrisers_Hyderabad_Logo.svg",
     "ipl": "https://upload.wikimedia.org/wikipedia/en/8/84/Indian_Premier_League_Official_Logo.svg"
 }
