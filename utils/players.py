027c410b0c5e8138445926a32c253100bdc4d444
Kalyan Teja <kalyan.bulla@digitalturbine.com>
2026-03-06 18:01:18 +0100

    added performence graph

--- utils/players.py
+++ utils/players.py
@@ -8,14 +8,14 @@ player_map :dict = {
     "subbareddy9599@gmail.com": "subba"
 }
 
-player_images : dict = {
-    "kalyan": "https://www.w3schools.com/howto/img_avatar.png",
-    "jaggu" :"https://www.w3schools.com/howto/img_avatar.png",
-    "subba" : "https://www.w3schools.com/howto/img_avatar.png",
-    "balu": "https://www.w3schools.com/howto/img_avatar.png",
-    "sravan" : "https://www.w3schools.com/howto/img_avatar.png",
-    "darsi" : "https://www.w3schools.com/howto/img_avatar.png"
-}
+# player_images : dict = {
+#     "kalyan": "https://www.w3schools.com/howto/img_avatar.png",
+#     "jaggu" :"https://www.w3schools.com/howto/img_avatar.png",
+#     "subba" : "https://www.w3schools.com/howto/img_avatar.png",
+#     "balu": "https://www.w3schools.com/howto/img_avatar.png",
+#     "sravan" : "https://www.w3schools.com/howto/img_avatar.png",
+#     "darsi" : "https://www.w3schools.com/howto/img_avatar.png"
+# }
 
 
 player_emails: list = list(player_map.keys())
