2b0a08d111b1cade2ed0d24e01846f3e41c16f27
Kalyan Teja <kalyan.bulla@digitalturbine.com>
2026-03-06 14:31:22 +0100

    added performsnce graph

--- utils/prediction_daily.py
+++ utils/prediction_daily.py
@@ -9,7 +9,7 @@ def prediction_next_match():
     conn = st.connection("gsheets", type=GSheetsConnection)
     df_bets = conn.read(worksheet="2026_bets_raw", ttl=0)
     df_nxt = conn.read(worksheet="2026_next_match", ttl=0)
-
+    st.write(df_nxt)
     next_match = df_nxt['next_match'].iloc[0].tolist()
     st.write(next_match)
 
