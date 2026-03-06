f06b373a995535a4799bc447afc31003aa1baa94
Kalyan Teja <kalyan.bulla@digitalturbine.com>
2026-03-06 01:55:56 +0100

    added performsnce graph

--- utils/cumilative_graph.py
+++ utils/cumilative_graph.py
@@ -47,7 +47,13 @@ def performance_graph():
         hovermode="x unified"
     )
 
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig,
+                    use_container_width=True,
+                    config = {
+                        'staticPlot': False,
+                        'displayModeBar': False,
+                        'scrollZoom': False
+                    })
 
 
 
