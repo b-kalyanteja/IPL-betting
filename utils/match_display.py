e3f541f6e7fbab41939e807e3808ac15ef5971ca
Kalyan Teja <kalyan.bulla@digitalturbine.com>
2026-03-07 02:03:33 +0100

    added performence graph

--- utils/match_display.py
+++ utils/match_display.py
@@ -95,8 +95,8 @@ def display_matches():
 def display_match_afterstart():
     # 1. Fetch all data
     df_today, df_schedule = cached_bet_data()
-    connection = st.connection("gsheets", type=GSheetsConnection)
-    df_logs = connection.read(worksheet="2026_bets_raw", ttl=0)
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    df_logs = conn.read(worksheet="2026_bets_raw", ttl=0)
 
     india_tz = pytz.timezone('Asia/Kolkata')
     current_time_str = datetime.now(india_tz).strftime("%H:%M")
