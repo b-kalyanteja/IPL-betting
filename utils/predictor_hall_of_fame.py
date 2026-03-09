f4a6a4689505c41c4af087bae29bdb5c0c12ccf9
Kalyan Teja <kalyan.bulla@digitalturbine.com>
2026-03-09 16:04:20 +0100

    added performence graph

--- utils/predictor_hall_of_fame.py
+++ utils/predictor_hall_of_fame.py
@@ -31,7 +31,7 @@ def predictor_stats():
 
     ttl_w = prediction_results.count('w')
     ttl_a: int = len([x for x in prediction_results if x and str(x).strip().lower() not in ["", "nan", "none", ".", "yet_to"]])
-    percent_win = ((ttl_w /ttl_a) * 100) if ttl_a > 0 else 0
+    percent_win: float = ((ttl_w /ttl_a) * 100) if ttl_a > 0 else 0
     percent_2026 = f"{percent_win:.1f}"
 
     return values_2026, percent_2026, percent_win
@@ -52,7 +52,7 @@ def hall_of_fame(img_file_name):
 
     with col2:
 
-        st.markdown(f"### 📅 2026 Season : {percent_2026:.1f}%")
+        st.markdown(f"### 📅 2026 Season : {percent_2026}%")
         st.write(values_2026)
 
         st.markdown("### 📅 2025 Season : 56.4%")
